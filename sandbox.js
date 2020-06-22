d33cd27254f7e37ae98af8ad91f324a328b457dd
Daniel Gil <danielgilucsd@gmail.com>
2020-06-22 11:22:16 -0700

    recipe funcion created to add documents from website to firestore database

---
+++ sandbox.js
@@ -0,0 +1,47 @@
+const list = document.querySelector('ul');
+const form = document.querySelector('form');
+
+const addRecipe = (recipe) => {
+    let time = (recipe.created_at.toDate());
+    let html = `
+    
+      <li>
+        <div>${recipe.title}</div>
+        <div>${time}</div>
+      </li>
+    
+    `;
+
+    list.innerHTML += html;
+ 
+}
+db.collection('recipes').get().then((snapshot) => {
+// when we  have the data
+    snapshot.docs.forEach(doc => {
+        console.log(doc.data())
+      addRecipe(doc.data());
+    })
+}).catch(err => {
+    console.log(err);
+})
+
+
+// add documents
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+
+const now = new Date();
+const recipe = {
+    title: form.recipe.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now)
+};
+
+db.collection('recipes').add(recipe).then(() => {
+    console.log('recipe added');
+}).catch(err => {
+    console.log(err);
+});
+
+});
